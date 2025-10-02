1ee6934aaaa51cfe66d952e619bdc352d95912e0
Aaron Bengochea <71156554+aaronbengochea@users.noreply.github.com>
2025-10-01 20:23:53 -0400

    adding support for google custom search JSON api automated seed loading

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # main.py
-from config import SEEDS, MAX_PAGES, MAX_WORKERS, LOG_FILE
+from config import MAX_PAGES, MAX_WORKERS, LOG_FILE
+from seed_fetcher import fetch_google_seeds
 from crawler import PriorityCrawler
 from logger import save_log
 
 if __name__ == "__main__":
-    crawler = PriorityCrawler(SEEDS, max_pages=MAX_PAGES, max_workers=MAX_WORKERS)
+    seeds = fetch_google_seeds()
+    print("Seeds: " ,seeds, "\n")
+    crawler = PriorityCrawler(seeds, max_pages=MAX_PAGES, max_workers=MAX_WORKERS)
     crawler.crawl()
     save_log(LOG_FILE, crawler.log, crawler.total_bytes, crawler.start_time, crawler.error_counts)
