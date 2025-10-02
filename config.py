8cff71b50032ff0ed990a041ac0ac4b3df05426e
Aaron Bengochea <71156554+aaronbengochea@users.noreply.github.com>
2025-10-01 20:22:47 -0400

    integrating google custom json search api and .env for key and cx variable loading

--- config.py
+++ config.py
@@ -1,23 +1,17 @@
-# Configuration for the web crawler
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-MAX_PAGES = 1000           # Total pages to crawl
-MAX_WORKERS = 50            # Number of concurrent worker threads
-FETCH_TIMEOUT = 1           # Seconds for fetching HTML pages
-ROBOTS_TIMEOUT = 3          # Seconds for fetching robots.txt
-LOG_FILE = "crawl_log_query2_test_15000x30.txt"
+SEED_QUERY = "how are tires made"
+NUMBER_OF_SEEDS = 10                    # Number of seed URLs to fetch via Google Custom Search API
+MAX_PAGES = 50                         # Total pages to crawl
+MAX_WORKERS = 50                        # Number of concurrent worker threads
+FETCH_TIMEOUT = 1                       # Seconds for fetching HTML pages
+ROBOTS_TIMEOUT = 3                      # Seconds for fetching robots.txt
+LOG_FILE = "crawl_log_DemoSample.txt"
 
-SEEDS = [
-    "https://www.ustires.org/tires-101/how-tire-made",
-    "https://www.michelinman.com/auto/auto-tips-and-advice/tires-101/how-are-tires-made",
-    "https://www.maxxis.com/us/technology/how-a-tire-is-made/",
-    "https://www.gotodobbs.com/blog/tires-construction-size/",
-    "https://www.goodyear.com/en_US/learn/tire-basics/how-are-tires-made.html",
-    "https://www.ace-laboratories.com/how-are-tires-made/",
-    "https://na.nokiantyres.com/tips/choosing-your-tires/how-are-tires-made/",
-    "https://www.elitetireandautoservice.net/About/News/ArticleID/17888",
-    "https://www.valoroffroad.com/blogs/the-source/how-is-a-tire-made?srsltid=AfmBOoqYUlvwa_7RzeCDxxBzEZvg2d4Bm8EgOEg5mhn-Ae3KElarbiLC",
-    "https://www.nexentire.com/international/information/tire_information/basic_sense/process.php",
-]
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_CX = os.getenv("GOOGLE_CX")
 
 BLACKLIST_EXT = (
     ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg",
